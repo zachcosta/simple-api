c2a31205d0b8d4581d3ba88233bcd4739b260f1b
zachc <zachcosta315@gmail.com>
2025-01-07 19:47:30 -0800

    Resolve Webpack Node Module issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ import path from 'path';
 
 const module = {
   mode: 'development',
-  entry: './app.js',
+  entry: {
+    main: './app.js',
+    htmlindex: './src/index.js'
+  },
   output: {
     path: path.resolve("__dirname", 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
   devServer: {
     static: {
@@ -14,6 +17,24 @@ const module = {
     compress: true,
     port: 9000,
   },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "url": false,
+      "util": false,
+      "querystring": false,
+      "crypto": false,
+      "os": false,
+      "async_hooks": false
+    } 
+  }
 };
 
 export default module
