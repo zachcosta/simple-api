5ead0756dbb87d9c86aa4e18503dfdc05a3cda0c
zachc <zachcosta315@gmail.com>
2025-01-07 20:12:08 -0800

    Testing something

--- webpack.config.js
+++ webpack.config.js
@@ -17,24 +17,24 @@ const module = {
     compress: true,
     port: 9000,
   },
-  resolve: {
-    fallback: {
-      "fs": false,
-      "tls": false,
-      "net": false,
-      "path": false,
-      "zlib": false,
-      "http": false,
-      "https": false,
-      "stream": false,
-      "url": false,
-      "util": false,
-      "querystring": false,
-      "crypto": false,
-      "os": false,
-      "async_hooks": false
-    } 
-  }
+  // resolve: {
+  //   fallback: {
+  //     "fs": false,
+  //     "tls": false,
+  //     "net": false,
+  //     "path": false,
+  //     "zlib": false,
+  //     "http": false,
+  //     "https": false,
+  //     "stream": false,
+  //     "url": false,
+  //     "util": false,
+  //     "querystring": false,
+  //     "crypto": false,
+  //     "os": false,
+  //     "async_hooks": false
+  //   } 
+  // }
 };
 
 export default module
