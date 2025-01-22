9b43de1d955436a361e468d83a5c4ba965af0ec9
zachc <zachcosta315@gmail.com>
2025-01-21 17:56:41 -0800

    Add modules pathing to Webpack build file

--- webpack.config.js
+++ webpack.config.js
@@ -17,24 +17,29 @@ const module = {
     compress: true,
     port: 9000,
   },
-  // resolve: {
-  //   fallback: {
-  //     "fs": false,
-  //     "tls": false,
-  //     "net": false,
-  //     "path": false,
-  //     "zlib": false,
-  //     "http": false,
-  //     "https": false,
-  //     "stream": false,
-  //     "url": false,
-  //     "util": false,
-  //     "querystring": false,
-  //     "crypto": false,
-  //     "os": false,
-  //     "async_hooks": false
-  //   } 
-  // }
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "url": false,
+      "util": false,
+      "querystring": false,
+      "crypto": false,
+      "os": false,
+      "async_hooks": false
+    }, 
+    // modules: [
+    //   path.resolve("...", 'src/lib/utils'),
+    //   path.resolve("__dirname", 'src/lib/models'),
+    //   path.resolve("__dirname", 'node_modules')
+    // ]
+  }
 };
 
 export default module
